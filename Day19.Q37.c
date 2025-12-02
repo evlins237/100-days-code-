64374e56bdbabf609bc4e3998e6c9f9f243e1571
Evlin s <evlins237@gmail.com>
2025-12-02 12:38:17 +0530

    Update and rename Day 19 to Day19.Q37.c

--- Day19.Q37.c
+++ Day19.Q37.c
@@ -26,22 +26,3 @@ int main() {
 }
 
 
-Q38: Write a program to find the sum of digits of a number.
-
-#include <stdio.h>
-
-int main() {
-    int number, sum = 0;
-
-    printf("Enter a number: ");
-    scanf("%d", &number);
-
-    while (number > 0) {
-        sum += number % 10;  
-        number = number / 10; 
-    }
-
-    printf("%d\n", sum);
-
-    return 0;
-}
